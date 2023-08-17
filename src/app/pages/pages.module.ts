362527f0a6a7dc9d1b75ebeebfb800dda70f1263
SantiagoOscarVera <santiagovera280@gmail.com>
2023-08-17 15:40:23 -0300

    creacion de rutas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LandingComponent } from './landing/landing.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingComponent,
+    DashboardComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LandingComponent,
+    DashboardComponent
   ]
 })
 export class PagesModule { }
