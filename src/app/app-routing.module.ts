7ad54bef06020f9616cd823adb01ba52d0c171fb
SantiagoOscarVera <santiagovera280@gmail.com>
2023-09-20 19:56:39 -0300

    cambios en auth.guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './auth/login/login.component';
 import { CotizacionesComponent } from './pages/cotizaciones/cotizaciones.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { AuthGuard } from './services/auth.guard';
+
 
 const routes: Routes = [
   {path:"home", component:LandingComponent},
-  {path:"dashboard", component:DashboardComponent},
+  {path:"dashboard", component:DashboardComponent, canActivate:[AuthGuard]}, // el can activate activa el servicio de autentificacion y su proteccion
   {path:"cotizaciones", component: CotizacionesComponent},
   {path:"login", component:LoginComponent},
   {path:"", redirectTo:"/home", pathMatch:"full"}
